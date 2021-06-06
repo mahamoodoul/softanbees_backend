2f539a791ae4f0916ef665f1448a8d4fd55e2fad
Shakil <mahamodul.shakil11@gmail.com>
2021-06-06 14:37:06 +0600

    bug updated

--- apply/views.py
+++ apply/views.py
@@ -13,14 +13,16 @@ def send_info(request):
         if ApplyModel.objects.filter(email=email).exists():
             request.session['email'] = email
             email = request.POST.get('email')
-            message = 'Email already exsists !!'
+            # message = 'Email already exsists !!'
+            message = 'exist'
         else:
             name = request.POST.get('name')
             profession = request.POST.get('toggle')
             create_apply_object = ApplyModel.objects.create(name=name,email=email,profession=profession)
             create_apply_object.save()
             request.session['email'] = email
-            message = "Your Account Created Successfully"
+            # message = "Your Account Created Successfully"
+            message = "success"
         return JsonResponse({"message": message}, status=200)
 
 def send_email(request):
@@ -35,22 +37,3 @@ def send_email(request):
             request.session['email'] = email
             message = "Your Account Created Successfully"
         return JsonResponse({"message": message}, status=200)
-
-
-
-
-
-
-
-
-    # if request.method == 'POST':
-    #     name = request.POST['name']
-    #     email = request.POST['email']
-    #     profession = request.POST.get('toggle')
-    #     create_apply_object = ApplyModel.objects.create(name=name,email=email,profession=profession)
-    #     create_apply_object.save()
-    #
-    # template_name = 'home.html'
-    # # context = {"You Application has been accepted successfully"}
-    #
-    # return render(request,template_name,context={})
